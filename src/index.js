49967da847dc3e74dd11e3972274717b7098d0a8
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-27 12:28:06 -0600

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addingTask(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function addingTask(task){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'x'
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
